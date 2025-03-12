dae6cdc8a8c9c44af8c33d39067dbb548730c503
Yan Boyko <yanboyko21@gmail.com>
2025-03-12 18:39:55 +0300

    Add Pulse library as dependency

--- Package.swift
+++ Package.swift
@@ -1,21 +1,18 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
     name: "MUDKit",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MUDKit",
-            targets: ["MUDKit"]),
+        .library(name: "MUDKit", targets: ["MUDKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kean/Pulse", exact: "5.1.3")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MUDKit"),
-
+        .target(name: "MUDKit"),
     ]
 )
