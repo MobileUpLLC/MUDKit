9ad8d9665b8ad0f7580902ff99306a62c4d2de27
Yan Boyko <yanboyko21@gmail.com>
2025-03-17 16:27:12 +0300

    refactor

--- Sources/MUDKit/UI/MUDKitView.swift
+++ Sources/MUDKit/UI/MUDKitView.swift
@@ -1,22 +1,14 @@
-//
-//  MUDKitView.swift
-//  MUDKit
-//
-//  Created by Ян Бойко on 17.03.2025.
-//
-
 import SwiftUI
 
-struct MUDKitView: View {
-    var body: some View {
-        Text("MUDKit")
-            .font(.headline)
+public struct MUDKitView: View {
+    public var body: some View {
         NavigationView {
             List {
-                NavigationLink("Открыть Pulse") {
+                NavigationLink("Pulse") {
                     PulseView()
                 }
             }
+            .navigationTitle("MUDKit")
         }
     }
 }
