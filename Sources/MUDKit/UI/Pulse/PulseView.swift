7423b348c867ecdc713feb28dbc124b48db01bf4
Yan Boyko <yanboyko21@gmail.com>
2025-03-19 19:51:20 +0300

    fix comment

--- Sources/MUDKit/UI/Pulse/PulseView.swift
+++ Sources/MUDKit/UI/Pulse/PulseView.swift
@@ -3,12 +3,17 @@ import PulseUI
 import Pulse
 
 struct PulseView: View {
-    let store: LoggerStore
-    let mode: ConsoleMode
+    private let store: LoggerStore
+    private let mode: ConsoleMode
     
     var body: some View {
         ConsoleView()
             .closeButtonHidden()
+    }
+    
+    init(store: LoggerStore, mode: ConsoleMode) {
+        self.store = store
+        self.mode = mode
     }
 }
 
