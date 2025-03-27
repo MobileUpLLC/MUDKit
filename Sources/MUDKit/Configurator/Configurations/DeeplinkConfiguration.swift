3521848c06e696ae76051b1b879a7237f7f1b64d
Yan Boyko <yanboyko21@gmail.com>
2025-03-27 13:08:13 +0300

    UPUP-1025: Part 4

--- Sources/MUDKit/Configurator/Configurations/DeeplinkConfiguration.swift
+++ Sources/MUDKit/Configurator/Configurations/DeeplinkConfiguration.swift
@@ -2,4 +2,8 @@ import Foundation
 
 public struct DeeplinkConfiguration {
     let deeplinkHandler: (URL) -> Void
+    
+    public init(deeplinkHandler: @escaping (URL) -> Void) {
+        self.deeplinkHandler = deeplinkHandler
+    }
 }
