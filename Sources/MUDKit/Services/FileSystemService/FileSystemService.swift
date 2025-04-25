8576f6ca649bc3d0d6a65f43da430158b05dc210
Ilia Chub <ilia_chub@icloud.com>
2025-04-25 17:43:24 +0300

    Simplify FileSystemService

--- Sources/MUDKit/Services/FileSystemService/FileSystemService.swift
+++ Sources/MUDKit/Services/FileSystemService/FileSystemService.swift
@@ -1,81 +1,57 @@
 import Foundation
 
 final class FileSystemService {
-    private let fileManager = FileManager.default
-    
-    func loadDirectory(
-        _ type: FileSystemServiceDirectoryType
-    ) throws -> (
-        type: FileSystemServiceDirectoryType,
-        directoryUrl: URL,
-        files: [URL]
-    ) {
-        let directoryUrl = constructDirectoryPath(type)
-        
-        do {
-            let directoryType = getDirectoryType(directoryUrl)
-            
-            let directoryContents = try fileManager.contentsOfDirectory(
-                at: directoryUrl,
-                includingPropertiesForKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey],
-                options: [.skipsHiddenFiles]
-            )
-            
-            return (type: directoryType, directoryUrl: directoryUrl, files: directoryContents)
-        } catch {
-            Log.fileSystemService.error(
-                logEntry: .text(FileSystemServiceError.loadDirectoryError.message + ": \(directoryUrl)")
-            )
-            
-            throw FileSystemServiceError.loadDirectoryError
+    func getContentOfUrl(_ url: URL) -> [URL]? {
+        var isDirectory: ObjCBool = false
+
+        guard
+            FileManager.default.fileExists(atPath: url.path, isDirectory: &isDirectory),
+            isDirectory.boolValue
+        else {
+            return nil
+        }
+
+        return try? FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: [])
+    }
+
+    func constructPath(
+        fileDirectory: FileSystemServiceDirectoryType,
+        fileName: String?,
+        fileExtension: String?
+    ) -> URL? {
+        guard var url = constructDirectoryPath(fileDirectory) else {
+            return nil
+        }
+
+        if let fileName {
+            url = url.appendingPathComponent(fileName)
+        }
+
+        if let fileExtension {
+            url = url.appendingPathExtension(fileExtension)
+        }
+
+        return url
+    }
+
+    private func constructDirectoryPath(_ directory: FileSystemServiceDirectoryType) -> URL? {
+        switch directory {
+        case .documents:
+            guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+                return nil
+            }
+
+            return url
+        case .temporary:
+            return URL(fileURLWithPath: FileManager.default.temporaryDirectory.path, isDirectory: true)
         }
     }
     
     func deleteFile(at url: URL) -> Bool {
-        guard fileManager.fileExists(atPath: url.path) else {
-            Log.fileSystemService.error(
-                logEntry: .text(FileSystemServiceError.existFileError.message + ": \(url)")
-            )
-            
-            return false
-        }
-        
-        do {
-            try fileManager.removeItem(at: url)
-            
+        guard FileManager.default.fileExists(atPath: url.path) else {
             return true
-        } catch {
-            Log.fileSystemService.error(
-                logEntry: .text(FileSystemServiceError.deleteFileError.message + ": \(url)")
-            )
-            
-            return false
         }
-    }
-    
-    private func constructDirectoryPath(_ directory: FileSystemServiceDirectoryType) -> URL {
-        switch directory {
-        case .documents:
-            guard let url = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
-                return constructDirectoryPath(.temporary)
-            }
-            
-            return url
-        case .temporary:
-            return URL(fileURLWithPath: fileManager.temporaryDirectory.path, isDirectory: true)
-        case .specific(url: let url):
-            return url
-        }
-    }
-    
-    // Для подмены типа директории в случае .specific
-    private func getDirectoryType(_ url: URL) -> FileSystemServiceDirectoryType {
-        if url == constructDirectoryPath(.documents) {
-            return .documents
-        } else if url == constructDirectoryPath(.temporary) {
-            return .temporary
-        } else {
-            return .specific(url: url)
-        }
+
+        return (try? FileManager.default.removeItem(at: url)) != nil
     }
 }
--- Sources/MUDKit/UI/FileSystem/FileSystemView.swift
+++ Sources/MUDKit/UI/FileSystem/FileSystemView.swift
@@ -7,7 +7,7 @@ struct FileSystemView: View {
     }
     
     @State private var files: [URL] = []
-    @State private var currentDirectory: FileSystemServiceDirectoryType?
+//    @State private var currentDirectory: FileSystemServiceDirectoryType?
     @State private var currentDirectoryUrl: URL?
     @State private var selectedFile: IdentifiableURL?
     @State private var errorMessage = ""
@@ -19,10 +19,10 @@ struct FileSystemView: View {
         VStack {
             HStack {
                 Group {
-                    Button(FileSystemServiceDirectoryType.documents.name) {
+                    Button("Documents") {
                         loadDirectory(type: .documents)
                     }
-                    Button(FileSystemServiceDirectoryType.temporary.name) {
+                    Button("Temporary") {
                         loadDirectory(type: .temporary)
                     }
                 }
@@ -35,41 +35,41 @@ struct FileSystemView: View {
                     .foregroundColor(.gray)
                     .padding()
             }
-            List {
-                if case let .specific(url) = currentDirectory {
-                    Button {
-                        loadDirectory(type: .specific(url: url.deletingLastPathComponent()))
-                    } label: {
-                        Image(systemName: "chevron.left")
-                    }
-                }
-                ForEach(files, id: \.self) { url in
-                    HStack {
-                        Button {
-                            if isDirectory(url) {
-                                loadDirectory(type: .specific(url: url))
-                            } else {
-                                selectedFile = IdentifiableURL(url: url)
-                            }
-                        } label: {
-                            VStack(alignment: .leading, spacing: 4) {
-                                HStack {
-                                    Image(systemName: isDirectory(url) ? "folder" : "doc")
-                                    Text(url.lastPathComponent)
-                                }
-                            }
-                        }
-                        .buttonStyle(.plain)
-                        Spacer()
-                        if isDirectory(url) == false {
-                            Button("Delete") {
-                                deleteFile(at: url)
-                            }
-                            .foregroundColor(.red)
-                        }
-                    }
-                }
-            }
+//            List {
+//                if case let .specific(url) = currentDirectory {
+//                    Button {
+//                        loadDirectory(type: .specific(url: url.deletingLastPathComponent()))
+//                    } label: {
+//                        Image(systemName: "chevron.left")
+//                    }
+//                }
+//                ForEach(files, id: \.self) { url in
+//                    HStack {
+//                        Button {
+//                            if isDirectory(url) {
+//                                loadDirectory(type: .specific(url: url))
+//                            } else {
+//                                selectedFile = IdentifiableURL(url: url)
+//                            }
+//                        } label: {
+//                            VStack(alignment: .leading, spacing: 4) {
+//                                HStack {
+//                                    Image(systemName: isDirectory(url) ? "folder" : "doc")
+//                                    Text(url.lastPathComponent)
+//                                }
+//                            }
+//                        }
+//                        .buttonStyle(.plain)
+//                        Spacer()
+//                        if isDirectory(url) == false {
+//                            Button("Delete") {
+//                                deleteFile(at: url)
+//                            }
+//                            .foregroundColor(.red)
+//                        }
+//                    }
+//                }
+//            }
         }
         .navigationTitle("File System")
         .sheet(item: $selectedFile) { identifiableURL in
@@ -85,40 +85,36 @@ struct FileSystemView: View {
     }
     
     private func loadDirectory(type: FileSystemServiceDirectoryType) {
-        do {
-            let (directoryType, directoryUrl, files) = try fileSystemService.loadDirectory(type)
-            self.currentDirectory = directoryType
-            self.currentDirectoryUrl = directoryUrl
-            self.files = files
-        } catch {
-            showErrorMessage(error)
+        guard
+            let directoryUrl = fileSystemService.constructPath(fileDirectory: type, fileName: nil, fileExtension: nil),
+            let content = fileSystemService.getContentOfUrl(directoryUrl)
+        else {
+            return showErrorMessage("Невозможно загрузить директорию \(type)")
         }
+
+        currentDirectoryUrl = directoryUrl
+        files = content
     }
     
-    private func deleteFile(at url: URL) {
-        guard let currentDirectory else {
-            return
-        }
-        
-        if fileSystemService.deleteFile(at: url) {
-            do {
-                let (_, _, files) = try fileSystemService.loadDirectory(currentDirectory)
-                self.files = files
-            } catch {
-                showErrorMessage(error)
-            }
-        } else {
-            showErrorMessage(FileSystemServiceError.deleteFileError)
-        }
-    }
+//    private func deleteFile(at url: URL) {
+//        guard let currentDirectory else {
+//            return
+//        }
+//        
+//        if fileSystemService.deleteFile(at: url) {
+//            do {
+//                let (_, _, files) = try fileSystemService.loadDirectory(currentDirectory)
+//                self.files = files
+//            } catch {
+//                showErrorMessage(error)
+//            }
+//        } else {
+//            showErrorMessage(FileSystemServiceError.deleteFileError)
+//        }
+//    }
     
-    private func showErrorMessage(_ error: Error) {
-        if let fileSystemServiceError = error as? FileSystemServiceError {
-            errorMessage = fileSystemServiceError.message
-        } else {
-            errorMessage = error.localizedDescription
-        }
-        
+    private func showErrorMessage(_ message: String) {
+        errorMessage = message
         isErrorShown = true
     }
 }
