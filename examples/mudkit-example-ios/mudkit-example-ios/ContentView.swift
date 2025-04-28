826fdb0d361eaea294379d6f212d9d3280077353
maria.nesterova <maria.nesterova@mobileup.ru>
2025-04-28 19:23:20 +0300

    [UPUP-1084]: fix comments

--- examples/mudkit-example-ios/mudkit-example-ios/ContentView.swift
+++ examples/mudkit-example-ios/mudkit-example-ios/ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
     var body: some View {
         MUDKitView()
             .onAppear {
-                MUDKitService.createSampleFilesForFileSystemServiceDemo()
+                FileSampleService.createSampleFiles()
             }
     }
 }
